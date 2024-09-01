549c94f25add23ce8d869a96cac4f1a0bb2f508f
Artem Balan <a.s.balan96@gmail.com>
2024-09-01 16:38:26 +0300

    197 task

--- src/main/java/org/example/code.sql
+++ src/main/java/org/example/code.sql
@@ -1,4 +1,10 @@
 -- 175 Combine two tables
-select p.firstName, p.lastName, a.city, a.state from
-Person as p left join Address as a on p.personId = a.personId;
+select p.firstName, p.lastName, a.city, a.state
+from Person as p left join Address as a
+on p.personId = a.personId;
 
+-- 197 Rising Temperature
+select b.id
+from Weather a, Weather b
+where datediff(b.recordDate, a.recordDate) = 1
+and b.temperature > a.temperature
