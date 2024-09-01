27842ae55f0c0c0eb41b3c72095fa5bb42f7f017
Artem Balan <a.s.balan96@gmail.com>
2024-09-01 18:57:02 +0300

    181 task

--- src/main/java/org/example/code.sql
+++ src/main/java/org/example/code.sql
@@ -3,14 +3,24 @@ select p.firstName, p.lastName, a.city, a.state
 from Person as p left join Address as a
 on p.personId = a.personId;
 
--- 197. Rising Temperature
-select b.id
-from Weather a, Weather b
-where datediff(b.recordDate, a.recordDate) = 1
-and b.temperature > a.temperature
+-- 181. Employees Earning More Than Their Managers
+select e1.name as Employee
+from Employee as e1
+inner join Employee as e2
+on e1.managerId = e2.id
+and e1.salary > e2.salary
 
 -- 182. Duplicate Emails
 select email as Email
 from Person
 group by email
 having COUNT(*) > 1
+
+-- 197. Rising Temperature
+select b.id
+from Weather a, Weather b
+where datediff(b.recordDate, a.recordDate) = 1
+and b.temperature > a.temperature
+
+
+
